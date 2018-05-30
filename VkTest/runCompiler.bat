8ff0a79db3df95501d4da18d072654f822e0fe4f
luxn <maximilian.herbers@outlook.com>
2018-05-30 23:01:15 +0200

    first commit

---
+++ VkTest/runCompiler.bat
@@ -0,0 +1,6 @@
+C:\VulkanSDK\1.1.73.0\Bin32\glslangValidator.exe -V shader.vert
+C:\VulkanSDK\1.1.73.0\Bin32\glslangValidator.exe -V shader.frag
+pause
+
+REM C:\VulkanSDK\1.1.73.0\Bin32\glslangValidator.exe -H shader.vert
+REM V ist binär und H ist für human readable
